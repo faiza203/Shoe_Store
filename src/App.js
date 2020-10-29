6090b20c2adbbe330f068ca49b205df81a149b8a
faiza203 <fiazfaiza981@gmail.com>
2020-10-29 14:10:07 +0500

    Resolve an error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import {BrowserRotuer as Router} from "react-router-dom"
+import {BrowserRouter as Router} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-     I am working
-    </div>
+    <Router></Router>
   );
 }
 
